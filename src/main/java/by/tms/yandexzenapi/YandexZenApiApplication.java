cffdd91aeda0d6815e6e979bc0a931c61a599315
HannaLa1 <anya.ladutko@mail.ru>
2022-01-18 00:20:46 +0300

    Initial commit

---
+++ src/main/java/by/tms/yandexzenapi/YandexZenApiApplication.java
@@ -0,0 +1,13 @@
+package by.tms.yandexzenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexZenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexZenApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/tms/yandexzenapi/YandexZenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.yandexzenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexZenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
